db0a26970b9c60a44b5a60ea244967729280d5a2
greg <varankov@list.ru>
2018-09-28 14:56:05 +0300

    training

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    if(str.length % 2 !== 0) return false;
+    var br = ''; 
+    for(var h=0; h<bracketsConfig.length; h++){
+        for(var n=0; n<2; n++){
+            br+= bracketsConfig[h][n];
+        }
+    }
+    var st = [];
+    for (var i = 0; i < str.length; ++i) {
+        var ch = str[i];
+        var ind = br.indexOf(ch);
+        if (ind >= 0) {
+            if (ind & 1) {
+                if (!st.length) return false;
+                var last_br = st.pop();
+                if (last_br != br[ind - 1]) return false;
+            } 
+            else { 
+                st.push(ch);
+            }
+        }
+    }
+    return true;
 }
